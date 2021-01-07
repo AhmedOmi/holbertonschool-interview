c1d0bc2537d26467432c70a3b8e89019e9c15bcf
ahmedomi <ahmedomarmiledi@gmail.com>
2021-01-07 16:19:56 +0100

    update and fixed errors

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,6 +15,6 @@ def minOperations(n):
     if n < 2:
         return 0
     else:
-        for count in range(2, n + 1):
-            if n % count == 0:
-                return minOperations(n // count) + count
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
