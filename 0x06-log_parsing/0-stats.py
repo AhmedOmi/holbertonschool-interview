00309367204de117f8abb7d01d4f71b859b88bc3
ahmedomi <ahmedomarmiledi@gmail.com>
2021-03-03 12:07:39 +0100

    add new interview tasks

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/python3
+""" Module that parse a log """
+import sys
+
+
+count = 0
+codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+file_size = 0
+
+try:
+    for line in sys.stdin:
+        list_line = line.split()
+        if len(list_line) > 2:
+            count += 1
+            code = list_line[-2]
+            line_size = int(list_line[-1])
+            if code in codes_dict:
+                codes_dict[code] += 1
+            file_size += line_size
+        if count % 10 == 0:
+            print('File size: {}'.format(file_size))
+            for k, v in sorted(codes_dict.items()):
+                if v > 0:
+                    print('{}: {}'.format(k, v))
+finally:
+    print('File size: {}'.format(file_size))
+    for k, v in sorted(codes_dict.items()):
+        if v > 0:
+            print('{}: {}'.format(k, v))
---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module to determine if a data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """
+    A character in UTF-8 can be 1 to 4 bytes long
+    The data set can contain multiple characters
+    The data will be represented by a list of integers
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    counter = 0
+    bins = [bin(b).replace('0b', '').rjust(8, '0') for b in data]
+    for b in bins:
+        if counter != 0:
+            counter -= 1
+            if b[:2] != '10':
+                return False
+        elif b[0] == '1':
+            counter = len(b.split('0')[0]) - 1
+    return True
