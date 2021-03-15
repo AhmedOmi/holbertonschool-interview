5e8dd3c1a57f22f3068a2df74b845de476b73cee
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-03-15 10:35:03 +0100

    add challenge Queens puzzle functions

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard.
+Write a program that solves the N queens problem.
+"""
+
+
+import sys
+
+if __name__ == '__main__':
+    print("queens")
