cd53ae923b2ef90090eb2b7e0c1c751b243874c5
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-09-06 11:40:14 +0100

    update and fixed betty style

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -7,14 +7,14 @@
  */
 int max(int *array, int size)
 {
-    int maximum, i;
-    maximum = array[0];
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > maximum)
-            maximum = array[i];
-    }
-    return (maximum);
+int maximum, i;
+maximum = array[0];
+for (i = 1; i < size; i++)
+{
+if (array[i] > maximum)
+maximum = array[i];
+}
+return (maximum);
 }
 
 /**
@@ -26,30 +26,30 @@ int max(int *array, int size)
  */
 void sort(int *array, int size, int index)
 {
-    int a, b, c, d, tmp, i;
-    a = 0;
-    while (a < size)
-    {
-        b = (array[a] / index) % 10;
-        c = a;
-        while (c < size)
-        {
-            d = (array[c] / index) % 10;
-            if (b > d)
-            {
-                tmp = array[c];
-                for (i = c - 1; i >= a; i--)
-                {
-                    array[i + 1] = array[i];
-                }
-                array[a] = tmp;
-                a = -1;
-                break;
-            }
-            c++;
-        }
-        a++;
-    }
+int a, b, c, d, tmp, i;
+a = 0;
+while (a < size)
+{
+b = (array[a] / index) % 10;
+c = a;
+while (c < size)
+{
+d = (array[c] / index) % 10;
+if (b > d)
+{
+tmp = array[c];
+for (i = c - 1; i >= a; i--)
+{
+array[i + 1] = array[i];
+}
+array[a] = tmp;
+a = -1;
+break;
+}
+c++;
+}
+a++;
+}
 }
 
 /**
@@ -60,16 +60,16 @@ void sort(int *array, int size, int index)
  */
 void radix_sort(int *array, size_t size)
 {
-    int a, maximum, i;
-    if (array == NULL || size < 2)
-        return;
-    a = (int)size;
-    maximum = max(array, a);
-    i = 1;
-    while ((maximum / i) > 0)
-    {
-        sort(array, a, i);
-        print_array(array, size);
-        i *= 10;
-    }
+int a, maximum, i;
+if (array == NULL || size < 2)
+return;
+a = (int)size;
+maximum = max(array, a);
+i = 1;
+while ((maximum / i) > 0)
+{
+sort(array, a, i);
+print_array(array, size);
+i *= 10;
+}
 }
