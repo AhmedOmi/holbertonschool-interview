16fbeacbab24234fa64bd15866673e274be73804
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-09-07 14:37:08 +0100

    fixed for boocle

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
     pascal triangel return triangle
     """
     t = [[0 for j in range(1, i + 2)] for i in range(n)]
-    for i in range(len()):
+    for i in range(len(t)):
         for j in range(len(t[i])):
             if i == 0 or j == 0 or j == i:
                 t[i][j] = 1
